7a7e60c414417594a2dd0609f8a4942f5b79fcbe
Clementina Calvo <cc@eiwa.ag>
2017-04-19 16:37:38 -0300

    Added exercices

---
+++ TP5b_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int a = 5;
+	void *p = &a;
+	void **pp = &p;
+
+	printf("pp:% p\n", pp);
+	printf("*pp:% p\n", *pp);
+	printf("**pp:% d", **pp);
+	
+	return 0;
+}
